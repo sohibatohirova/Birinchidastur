967c32d3717cd76a6cd4cc41c1925d6e1ff5b3ce
sohibatohirova <sohibatohirova22@gmail.com>
2024-11-07 11:59:20 +0500

    codeAdd

--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,15 @@ namespace src
     {
         static void Main(string[] args)
         {
+            Console.Write(" To'rt xonali son kiriting:");
+            var tortxona=Convert.ToInt32(Console.ReadLine());
+            int r1 = tortxona % 10;
+            int r2=tortxona / 10%10;
+            int r3=tortxona / 100%10;
+            int r4=tortxona / 1000%10;
+            Console.WriteLine(r1+r2+r3+r4);
+            Console.ReadLine();
+
         }
     }
 }
